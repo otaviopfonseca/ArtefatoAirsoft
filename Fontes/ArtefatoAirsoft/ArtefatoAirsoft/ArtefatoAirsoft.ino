5b7f406b84f04f2b4749cc6fca5b8c80987f2309
Otavio Fonseca <otaviopfonseca@yahoo.com.br>
2017-09-05 13:47:13 -0300

    Criação do projeto no visual studio

---
+++ Fontes/ArtefatoAirsoft/ArtefatoAirsoft/ArtefatoAirsoft.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArtefatoAirsoft.ino
+ Created:	9/5/2017 1:39:00 PM
+ Author:	DTI-Otavio
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	Serial.begin(9600);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	Serial.println("Teste artefato");  
+}
--- Fontes/BombaAirsoft/BombaAirsoft.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-}
-
-void loop() {
-  Serial.println("HELLO WORLD");
-  delay(1);
-}
